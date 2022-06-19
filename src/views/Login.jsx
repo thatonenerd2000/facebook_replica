109188e568074333ada459c4da1320c7adc416ab
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2022-06-19 19:02:37 -0400

    signup form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../style.scss'
 import fbLogo from "../media/fbLogo.png"
@@ -7,9 +7,11 @@ import Fade from 'react-reveal/Fade';
 
 
 const LoginForm = () => {
+    const [loginDisplay, setloginUpDisplay] = useState("block")
+    const [signUpDisplay, setsignUpDisplay] = useState("none")
     return(
         <>
-            <div id="loginForm">
+            <div style={{display:loginDisplay}} id="loginForm">
                 <img id="fbWrittenLogo" src={fbWrittenLogo}></img>
                 <br></br>
                 <input type="text" placeholder='Email address or phone number'></input>
@@ -21,7 +23,22 @@ const LoginForm = () => {
                 <a href="#">Forgotten password?</a>
                 <hr></hr>
                 <br></br>
-                <button id="createAcc">Create New Account</button>
+                <button id="createAcc" onClick={() => {setloginUpDisplay("none"); setsignUpDisplay("block")}}>Create New Account</button>
+            </div>
+
+            <div style={{display:signUpDisplay}} id="signupform">
+                <img id="fbWrittenLogo" src={fbWrittenLogo}></img>
+                <br></br>
+                <input type="text" placeholder='First Name'></input>
+                <input type="text" placeholder='Last Name'></input>
+                <br></br>
+                <input style={{width:'423px'}} type="text" placeholder='Email address'></input>
+                <br></br>
+                <input style={{width:'423px'}} type="password" placeholder='Password'></input>
+                <br></br>
+                <input style={{width:'423px'}} type="text" placeholder="Birthday: MM/DD/YYYY" onFocus={e => (e.target.type = "date")} onBlur={e => (e.target.type = "text")}></input>
+                <br></br>
+                <button id="SignUp">Sign Up</button>
             </div>
         </>
     )
