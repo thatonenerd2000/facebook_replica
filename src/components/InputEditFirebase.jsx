6fe2513d4a30f7fb7bd2545d75b98336c31a46f2
Shadman Chowdhury <chowdhuryshadman0@gmail.com>
2022-06-26 21:29:09 -0400

    Edit action bug fix

--- src/components/InputEditFirebase.jsx
+++ src/components/InputEditFirebase.jsx
@@ -15,33 +15,39 @@ import {ConfigContext} from '../GlobalContext';
 const InputEditFirebase = (props) => {
     const Globalconfig = useContext(ConfigContext)
 
+    useEffect(() => {
+        console.log(props.textValue)
+    },[props.textValue])
+
     if(props.textValue === ""){
         return(
-            <div id={props.id}>
-                <p className="hoverUnderline" id={props.id+"textEdit"} style={{display: props.textValue === "" ? "block" : "none",}} onClick={() => {
-                    document.getElementById(props.id+"textEdit").style.display = "none"
-                    document.getElementById(props.id+"textInput").style.display = "block"
-                    document.getElementById(props.id+"textSubmit").style.display = "block"
-                    document.getElementById(props.id+"textInput").focus()
-                }}>{props.editIcon} {props.inputText}</p>
-                <input id={props.id+"textInput"} type="text" placeholder='Add a bio' style={{display:'none'}}></input>
-                <Button id={props.id+"textSubmit"} variant="success" style={{display:'none'}} onClick={() => {
-                    let obj = {}
-                    // Push the bio to the database and update the user's info
-                    obj[props.fireBaseObjKey] = document.getElementById(props.id+"textInput").value
-                    update(ref(getDatabase(),props. fireBasePathToUpdate), obj)
-                    getUserInfo(Globalconfig.UID, getDatabase(), Globalconfig.setUserData)
-                    document.getElementById(props.id+"textEdit").remove()
-                    document.getElementById(props.id+"textInput").remove()
-                    document.getElementById(props.id+"textSubmit").remove()
-                }}>Save</Button>
-            </div>
+            <>
+                <div id={props.id}>
+                    <p className="hoverUnderline" id={props.id+"textEdit"} style={{display: props.textValue === "" ? "block" : "none",}} onClick={() => {
+                        document.getElementById(props.id+"textEdit").style.display = "none"
+                        document.getElementById(props.id+"textInput").style.display = "block"
+                        document.getElementById(props.id+"textSubmit").style.display = "block"
+                        document.getElementById(props.id+"textInput").focus()
+                    }}>{props.editIcon} {props.inputText}</p>
+                    <input id={props.id+"textInput"} type="text" placeholder='Add a bio' style={{display:'none'}}></input>
+                    <Button id={props.id+"textSubmit"} variant="success" style={{display:'none'}} onClick={() => {
+                        let obj = {}
+                        // Push the bio to the database and update the user's info
+                        obj[props.fireBaseObjKey] = document.getElementById(props.id+"textInput").value
+                        update(ref(getDatabase(),props. fireBasePathToUpdate), obj)
+                        getUserInfo(Globalconfig.UID, getDatabase(), Globalconfig.setUserData)
+                        // document.getElementById(props.id+"textEdit").remove()
+                        // document.getElementById(props.id+"textInput").remove()
+                        // document.getElementById(props.id+"textSubmit").remove()
+                    }}>Save</Button>
+                </div>
+            </>
         )
     }
     else{
         return(
             <div id={props.id}>
-                <p style={{display: props.textValue === "" ? "none" : "block"}}>{props.children}</p>
+                <p>{props.children}</p>
             </div>
         )
     }
